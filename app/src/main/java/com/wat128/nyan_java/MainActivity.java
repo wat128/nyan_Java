bc8155aea4144ca8ce26b0559692d56113ade0dd
wat128 <wat128sideb@gmail.com>
2020-06-11 19:42:45 +0900

    簡単なボタンアプリを作る

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -1,13 +1,39 @@
 package com.wat128.nyan_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        textView = findViewById(R.id.text_view);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    textView.setText(R.string.hello);
+                    flag = false;
+                } else {
+                    textView.setText(R.string.world);
+                    flag = true;
+                }
+            }
+        });
+
+
     }
 }
