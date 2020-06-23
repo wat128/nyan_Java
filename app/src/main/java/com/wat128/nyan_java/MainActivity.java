a735e9652cade39510094155c87c87009d5fbf33
wat128 <wat128sideb@gmail.com>
2020-06-23 17:24:55 +0900

    5.ProgressBar ProgressBarで進捗状況を表示する - 3

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -23,7 +23,7 @@ import com.squareup.picasso.Picasso;
 public class MainActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
-    private boolean flg = false;
+    private int val = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,18 +32,16 @@ public class MainActivity extends AppCompatActivity {
 
         progressBar = findViewById(R.id.progressbar);
 
+        progressBar.setMax(100);
+
         Button button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(flg){
-                    progressBar.setVisibility(ProgressBar.VISIBLE);
-                    flg = false;
-                } else {
-                    progressBar.setVisibility(ProgressBar.INVISIBLE);
-                    flg = true;
-                }
+                val += 10;
+                progressBar.setProgress(val);
+                progressBar.setSecondaryProgress(70);
             }
         });
 
