fbac883fd1683b1f701def50c637ac5180462802
wat128 <wat128sideb@gmail.com>
2020-07-02 20:39:36 +0900

    10.Spinner カスタマイズして画像リストを表示する

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -1,77 +1,50 @@
 package com.wat128.nyan_java;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.Display;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.SeekBar;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-import com.squareup.picasso.Picasso;
-
-import java.lang.reflect.Type;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] spinnerItems = {"Spinner", "Android", "Apple", "Windows"};
-    private TextView textView;
+
+    private static final String[] spinnerItems = {
+            "Select", "Image", "Image2"};
+
+    private static final String[] spinnerImages = {
+            "transparent", "image", "image2"};
+
+    private ImageView imageView;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text_view);
+        imageView = findViewById(R.id.image_view);
 
         Spinner spinner = findViewById(R.id.spinner);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinnerItems);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        TestAdapter adapter = new TestAdapter(this.getApplicationContext(),
+                R.layout.list, spinnerItems, spinnerImages);
 
         spinner.setAdapter(adapter);
 
+        // リスナーを登録
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Spinner spinner = (Spinner)parent;
-                String item = (String)spinner.getSelectedItem();
-                textView.setText(item);
+            //　アイテムが選択された時
+            public void onItemSelected(AdapterView<?> parent,
+                                       View viw, int position, long id) {
+
+                imageView.setImageResource(getResources().
+                        getIdentifier(spinnerImages[position],
+                                "drawable", getPackageName()));
             }
 
-            @Override
+            //　アイテムが選択されなかった
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
     }
