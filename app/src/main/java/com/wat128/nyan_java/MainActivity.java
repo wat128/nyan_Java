58263d82d86e1d68a52ad037937931ee40576902
wat128 <wat128sideb@gmail.com>
2020-07-03 19:51:51 +0900

    11.Activity Activity間のデータ受け渡し

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wat128.nyan_java;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,40 +8,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // the key constant
+    public static final String EXTRA_MESSAGE = "com.wat128.nyan_java.MESSAGE";
+
+    private TextView textView;
+    static final int RESULT_SUBACTIVITY = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setScreenMain();
-    }
-
-    private void setScreenMain() {
         setContentView(R.layout.activity_main);
 
-        Button sendButton = findViewById(R.id.send_button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        textView = findViewById(R.id.text_view);
+
+        final EditText editText = findViewById(R.id.edit_text);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               setScreenSub();
+                Intent intent = new Intent(getApplication(), SubActivity.class);
+                if(editText.getText() != null) {
+                    String str = editText.getText().toString();
+                    intent.putExtra(EXTRA_MESSAGE, str);
+                }
+                startActivityForResult(intent, RESULT_SUBACTIVITY);
+
+                editText.setText("");
             }
         });
     }
 
-    private void setScreenSub() {
-        setContentView(R.layout.activity_sub);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        Button returnButton = findViewById(R.id.return_button);
-        returnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setScreenMain();
-            }
-        });
+        if(resultCode == RESULT_OK && requestCode == RESULT_SUBACTIVITY && null != data) {
+            String res = data.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            textView.setText(res);
+        }
     }
 }
 
--- app/src/main/java/com/wat128/nyan_java/SubActivity.java
+++ app/src/main/java/com/wat128/nyan_java/SubActivity.java
@@ -1,24 +1,47 @@
 package com.wat128.nyan_java;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SubActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private String message;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_sub);
 
-        Button returnButton = findViewById(R.id.return_button);
-        returnButton.setOnClickListener(new View.OnClickListener() {
+        Intent intent = getIntent();
+        message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText(message);
+
+        editText = findViewById(R.id.edit_text);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent();
+                if(editText.getText() != null) {
+                    String str = message + editText.getText().toString();
+                    intent.putExtra(MainActivity.EXTRA_MESSAGE, str);
+                }
+
+                editText.setText("");
+
+                setResult(RESULT_OK, intent);
+
                 finish();
             }
         });
