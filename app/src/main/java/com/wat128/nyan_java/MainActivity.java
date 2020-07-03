f5eb6d4164ac7c4c2a8853fe5816a97b2b1de671
wat128 <wat128sideb@gmail.com>
2020-07-03 18:21:53 +0900

    11.Activity Activityの画面遷移 1

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -2,49 +2,27 @@ package com.wat128.nyan_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String[] spinnerItems = {
-            "Select", "Image", "Image2"};
-
-    private static final String[] spinnerImages = {
-            "transparent", "image", "image2"};
-
-    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.image_view);
-
-        Spinner spinner = findViewById(R.id.spinner);
-
-        TestAdapter adapter = new TestAdapter(this.getApplicationContext(),
-                R.layout.list, spinnerItems, spinnerImages);
-
-        spinner.setAdapter(adapter);
-
-        // リスナーを登録
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            //　アイテムが選択された時
-            public void onItemSelected(AdapterView<?> parent,
-                                       View viw, int position, long id) {
-
-                imageView.setImageResource(getResources().
-                        getIdentifier(spinnerImages[position],
-                                "drawable", getPackageName()));
-            }
-
-            //　アイテムが選択されなかった
-            public void onNothingSelected(AdapterView<?> parent) {
+        Button sendButton = findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), SubActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/wat128/nyan_java/SubActivity.java
@@ -0,0 +1,26 @@
+package com.wat128.nyan_java;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_sub);
+
+        Button returnButton = findViewById(R.id.return_button);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
