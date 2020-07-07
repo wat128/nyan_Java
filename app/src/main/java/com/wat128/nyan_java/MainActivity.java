3809207b595cc41747d1c83078cdf4d823d0945b
wat128 <wat128sideb@gmail.com>
2020-07-07 16:31:38 +0900

    13.Service WindowManagerを使ってServiceから画像を表示させ続ける

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -6,9 +6,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,30 +24,43 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int OVERLAY_PERMISSION_REQ_CODE = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
 
         Button buttonStart = findViewById(R.id.button_start);
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), TestService.class);
-                intent.putExtra("REQUEST_CODE", 1);
-
                 startForegroundService(intent);
             }
         });
+    }
 
-        Button buttonStop = findViewById(R.id.button_stop);
-        buttonStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), TestService.class);
-                stopService(intent);
+    @TargetApi(Build.VERSION_CODES.M)
+    public void checkPermission() {
+        if(!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if(!Settings.canDrawOverlays(this)) {
+                Log.d("debug", "SYSTEM_ALERT_WINDOWS permission not granted...");
             }
-        });
+        }
     }
 }
 
--- app/src/main/java/com/wat128/nyan_java/TestService.java
+++ app/src/main/java/com/wat128/nyan_java/TestService.java
@@ -7,9 +7,18 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.text.Layout;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.PixelCopy;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,101 +26,98 @@ import androidx.annotation.Nullable;
 
 public class TestService extends Service {
 
-    private MediaPlayer mediaPlayer;
-    private int counter = 0;
+    private View view;
+    private WindowManager windowManager;
+    private int dpScale ;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("debug", "onCreate()");
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.sample);
+        dpScale = (int)getResources().getDisplayMetrics().density;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        Log.d("debug", "onStartCommand()");
-
-        int requestCode = intent.getIntExtra("REQUEST_CODE", 0);
         Context context = getApplicationContext();
         String channelId = "default";
         String title = context.getString(R.string.app_name);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        NotificationChannel channel = new NotificationChannel(channelId, title, NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationChannel channel = new NotificationChannel( channelId, title , NotificationManager.IMPORTANCE_DEFAULT);
 
-        if(notificationManager != null) {
+        if(notificationManager != null){
+
             notificationManager.createNotificationChannel(channel);
 
             Notification notification = new Notification.Builder(context, channelId)
                     .setContentTitle(title)
-                    .setSmallIcon(android.R.drawable.ic_media_play)
-                    .setContentText("MediaPlay")
+                    .setSmallIcon(android.R.drawable.btn_star)
+                    .setContentText("APPLICATION_OVERLAY")
                     .setAutoCancel(true)
                     .setContentIntent(pendingIntent)
                     .setWhen(System.currentTimeMillis())
                     .build();
 
             startForeground(1, notification);
-
-            audioStart();
         }
 
-        return START_NOT_STICKY;
-    }
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
 
-    private void audioStart() {
-        ++counter;
-        Log.d("debug", "audioStart: " + String.valueOf(counter));
+        int typeLayer = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
 
-        if(mediaPlayer != null) {
-            mediaPlayer.setLooping(true);
+        windowManager = (WindowManager)getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
 
-            mediaPlayer.start();
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams (
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                typeLayer,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                PixelFormat.TRANSLUCENT);
 
-            String str = "Start Walking\n(c)Music-Note.jp";
-            Toast toast = Toast.makeText(this, str, Toast.LENGTH_LONG);
-            toast.show();
+        params.gravity=  Gravity.TOP | Gravity.END;
+        params.x = 20 * dpScale;
+        params.y = 80 * dpScale;
 
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    Log.d("debug", "end of audio");
+        final ViewGroup nullParent = null;
+        view = layoutInflater.inflate(R.layout.service_layer, nullParent);
 
-                    audioStop();
+        view.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("debug","onTouch");
 
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    Log.d("debug","ACTION_DOWN");
+                }
+
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    Log.d("debug","ACTION_UP");
+                    view.performClick();
                     stopSelf();
                 }
-            });
-        }
-    }
 
-    private void audioStop() {
-        mediaPlayer.stop();
-        mediaPlayer.reset();
-        mediaPlayer.release();
-        mediaPlayer = null;
+                return false;
+            }
+        });
+
+        windowManager.addView(view, params);
+
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-
-        Log.d("debug", "onDestroy()");
-
-        if(mediaPlayer != null) {
-            Log.d("debug", "end of audio");
-            audioStop();
-        }
-
-        stopSelf();
+        Log.d("debug","onDestroy");
+        windowManager.removeView(view);
     }
 
-    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
