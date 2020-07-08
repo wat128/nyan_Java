b4b6a4437d563f2453924044cecf1da6daa72a0e
wat128 <wat128sideb@gmail.com>
2020-07-08 17:36:16 +0900

    14.Picker DatePicker, TimePickerをDialogFragmentを使って実装 - DataPicker

---
+++ app/src/main/java/com/wat128/nyan_java/DataPick.java
@@ -0,0 +1,31 @@
+package com.wat128.nyan_java;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DataPick extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), (MainActivity)getActivity(), year, month, day);
+    }
+
+    @Override
+    public void onDateSet(android.widget.DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+    }
+
+}
--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -2,11 +2,14 @@ package com.wat128.nyan_java;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.TargetApi;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
@@ -17,29 +20,36 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
-    private Intent intent;
+public class MainActivity extends FragmentActivity implements DatePickerDialog.OnDateSetListener {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 開始ボタン
-        Button buttonStart = findViewById(R.id.button_start);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                intent = new Intent(MainActivity.this, TestIntentService.class);
-                startService(intent);
-            }
-        });
+        textView = findViewById(R.id.textView);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+        String str = String.format(Locale.US, "%d%d%d", year, month+1, dayOfMonth);
+        textView.setText(str);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DataPick();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
 
