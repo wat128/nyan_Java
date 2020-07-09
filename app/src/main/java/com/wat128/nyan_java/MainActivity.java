b94cbf0bbeb721cd37a87ec44c05c4f6727f1fdd
wat128 <wat128sideb@gmail.com>
2020-07-09 14:51:21 +0900

    15.ScrollView ScrollView 画面の縦スクロール - コードで記述

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -13,18 +13,23 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -36,8 +41,25 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
         Log.d("debug", "MainActivity.onCreate");
+
+        final int matchParent = ViewGroup.LayoutParams.MATCH_PARENT;
+        final int wrapContent = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+        ScrollView scrollView = new ScrollView(this);
+        scrollView.setLayoutParams(new ScrollView.LayoutParams(matchParent, wrapContent));
+
+        ImageView imageView = new ImageView(this);
+
+        imageView.setImageResource(R.drawable.image2);
+
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(matchParent, wrapContent));
+
+        scrollView.addView(imageView);
+
+        setContentView(scrollView);
+
     }
 }
 
