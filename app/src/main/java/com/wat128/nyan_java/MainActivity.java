c75074d4ce1b452693cae937486af5c430f9ec13
wat128 <wat128sideb@gmail.com>
2020-07-09 15:34:12 +0900

    15.ScrollView HorizontalScrollView 横スクロール - コードで記述

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -13,6 +13,8 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
@@ -26,9 +28,11 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -41,9 +45,46 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         Log.d("debug", "MainActivity.onCreate");
+
+        int MATCH_PARENT = ViewGroup.LayoutParams.MATCH_PARENT;
+        int WRAP_CONTENT = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+        RelativeLayout layout = new RelativeLayout(this);
+        layout.setLayoutParams(new RelativeLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT));
+
+        HorizontalScrollView scrollView = new HorizontalScrollView(this);
+        scrollView.setLayoutParams(new HorizontalScrollView.LayoutParams(MATCH_PARENT, MATCH_PARENT));
+
+        ImageView imageView = new ImageView(this);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image2);
+        imageView.setImageBitmap(bitmap);
+
+        imageView.setLayoutParams(new RelativeLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        scrollView.addView(imageView);
+        setContentView(scrollView);
+
+
+/* // サンプルみないで記述したコード
+        final int matchParent = ViewGroup.LayoutParams.MATCH_PARENT;
+        final int wrapContent = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+        HorizontalScrollView horizontalScrollView = new HorizontalScrollView(this);
+        horizontalScrollView.setLayoutParams(new HorizontalScrollView.LayoutParams(wrapContent, matchParent));
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.image2);
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(wrapContent, wrapContent));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        horizontalScrollView.addView(imageView);
+        setContentView(horizontalScrollView);
+*/ // サンプルみないで記述したコード
+
     }
 }
 
