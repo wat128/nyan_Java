35de1c0de9ffbc9316da6d2a36fc7cbfb7ea6fd8
wat128 <wat128sideb@gmail.com>
2020-07-10 16:04:58 +0900

    16.ListView ArrayAdapterを使ってレイアウトをアレンジ

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -3,8 +3,13 @@ package com.wat128.nyan_java;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends FragmentActivity {
 
@@ -18,16 +23,30 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         Log.d("debug", "MainActivity.onCreate");
 
-        ListView listView = new ListView(this);
-        setContentView(listView);
-
-        // simple_list_item_1は、デフォルトで用意されているレイアウトファイルのID
         ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<>(this, R.layout.list, texts);
+                new ArrayAdapter<>(this, R.layout.list);
+
+        ListView listView = findViewById(R.id.listview);
+
+        for(String str : texts) {
+            arrayAdapter.add(str);
+        }
 
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        String.format(Locale.US, "%sがTapされました", texts[position]),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
 
