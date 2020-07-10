13745484a10e6e135db76325d2345305d25d5b3e
wat128 <wat128sideb@gmail.com>
2020-07-10 15:21:40 +0900

    16.ListView ListViewとArrayAdapter 簡単なテキストリストの表示 - シンプルなArrayAdapter

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -1,53 +1,33 @@
 package com.wat128.nyan_java;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.annotation.TargetApi;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.NumberPicker;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.TimePicker;
-
-import java.util.Locale;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends FragmentActivity {
+
+    private static final String[] texts = {
+            "abc", "bcd", "cde", "def", "efg",
+            "fgh", "ghi", "hij", "ijk", "jkl",
+            "klm", "スクロールのテスト", "bbb", "ccc", "ddd",
+            "eee", "fff", "ggg", "hhh", "iii"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         Log.d("debug", "MainActivity.onCreate");
+
+        ListView listView = new ListView(this);
+        setContentView(listView);
+
+        // simple_list_item_1は、デフォルトで用意されているレイアウトファイルのID
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, texts);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
 
