1606617685be7915bcd2e332bee9d14df82b16e9
wat128 <wat128sideb@gmail.com>
2020-07-15 16:34:16 +0900

    19.AlertDialog AlertDialog : FragmentDialogを使って設定

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -2,17 +2,23 @@ package com.wat128.nyan_java;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.nfc.TagLostException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -23,33 +29,71 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private IconList icons;
-    private List<Integer> iconList = new ArrayList<>();
     private TextView textView;
+    private DialogFragment dialogFragment;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        icons = new IconList();
-        for(int i = 0; i < icons.maxNum(); ++i) {
-            iconList.add(icons.getIcon(i));
-        }
+        textView = findViewById(R.id.text_view);
 
-        GridView gridView = findViewById(R.id.grid_view);
+        Button button = findViewById(R.id.button);
 
-        GridAdapter adapter = new GridAdapter(this.getApplicationContext(), R.layout.grid_items, iconList);
-        gridView.setAdapter(adapter);
-        gridView.setOnItemClickListener(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager = getSupportFragmentManager();
 
-        textView = (TextView)findViewById(R.id.text_view);
+                dialogFragment = new AlertDialogFragment();
+
+                dialogFragment.show(fragmentManager, "test alert dialog");
+            }
+        });
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        textView.setText(icons.getName(position));
+    public void setTextView(final String message) {
+        textView.setText(message);
+    }
+
+    public static class AlertDialogFragment extends DialogFragment {
+
+        private String[] menulist = {"AAA", "BBB", "CCC"};
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            alert.setTitle("Test AlertDialog");
+            alert.setItems(menulist, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if(which == 0){
+                        setMessage(menulist[0]);
+                    }
+                    else if (which == 1) {
+                        setMessage(menulist[1]);
+                    }
+                    else {
+                        setMessage(menulist[2]);
+                    }
+                }
+            });
+
+            return alert.create();
+        }
+
+        private void setMessage(String message) {
+            MainActivity mainActivity = (MainActivity)getActivity();
+            if(mainActivity != null) {
+                mainActivity.setTextView(message);
+            }
+        }
     }
 }
