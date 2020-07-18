ee6b46603598bc132e22dde0a6fb02a930f32e1f
wat128 <wat128sideb@gmail.com>
2020-07-18 19:45:50 +0900

    20.Data Storage 外部ストレージにファイルを保存する WRITE EXTERNAL STORAGE

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -1,90 +1,178 @@
 package com.wat128.nyan_java;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import android.content.Context;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
-
 import android.widget.Button;
 import android.widget.EditText;
-
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private final int REQUEST_PERMISSION = 1000;
+
     private TextView textView;
-    private String fileName = "test.txt";
+    private EditText editText;
+    private File file;
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        File path = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        String testfile ="testfile.txt";
+        file = new File(path, testfile);
+
+        if (Build.VERSION.SDK_INT >= 23)
+            checkPermission();
+        else
+            setUpReadWriteExternalStorage();
+    }
+
+    private void setUpReadWriteExternalStorage(){
+
         textView = findViewById(R.id.text_view);
         editText = findViewById(R.id.edit_text);
-        Button buttonSave = findViewById(R.id.button_save);
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = editText.getText().toString();
 
-                saveFile(fileName, text);
-                if(text.length() == 0)
-                    textView.setText(R.string.no_text);
-                else
-                    textView.setText(R.string.saved);
-            }
+        Button buttonSave = findViewById(R.id.button_save);
+        buttonSave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                if(isExternalStorageWritable()) {
+                    String text = null;
+                    String str = editText.getText().toString();
+
+                    try(FileOutputStream fileOutputStream =
+                                new FileOutputStream(file, true);
+                        OutputStreamWriter outputStreamWriter =
+                                new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
+                        BufferedWriter bw =
+                                new BufferedWriter(outputStreamWriter);
+                    ) {
+
+                        bw.write(str);
+                        bw.flush();
+                        text = "saved";
+
+                    } catch (Exception e) {
+
+                        text = "error: FileOutputStream";
+                        e.printStackTrace();
+
+                    }
+
+                    textView.setText(text);
+                }
+            };
         });
 
         Button buttonRead = findViewById(R.id.button_read);
-        buttonRead.setOnClickListener(new View.OnClickListener() {
+        buttonRead.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                String str = readFile(fileName);
-                if(str != null)
-                    textView.setText(str);
-                else
-                    textView.setText(R.string.read_error);
-            }
+            public void onClick(View arg0) {
+                String str = null;
+
+                if(isExternalStorageReadable()) {
+
+                    try(FileInputStream fileInputStream =
+                                new FileInputStream(file);
+                        InputStreamReader inputStreamReader =
+                                new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+                        BufferedReader reader=
+                                new BufferedReader(inputStreamReader); ) {
+
+                        String lineBuffer;
+
+                        while( (lineBuffer = reader.readLine()) != null ) {
+                            str = lineBuffer ;
+                        }
+
+                    } catch (Exception e) {
+                        str = "error: FileInputStream";
+                        e.printStackTrace();
+                    }
+                }
+                textView.setText(str);
+            };
         });
     }
 
-    public void saveFile(String file, String str) {
+    public boolean isExternalStorageWritable() {
 
-        try(FileOutputStream fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)){
-            fileOutputStream.write(str.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String state = Environment.getExternalStorageState();
+        return (Environment.MEDIA_MOUNTED.equals(state));
+
     }
 
-    public String readFile(String file) {
-        String text = null;
+    public boolean isExternalStorageReadable() {
 
-        try (FileInputStream fileInputStream = openFileInput(file);
-             BufferedReader reader= new BufferedReader(
-                     new InputStreamReader(fileInputStream, StandardCharsets.UTF_8))) {
+        String state = Environment.getExternalStorageState();
+        return (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
 
-            String lineBuffer;
-            while( (lineBuffer = reader.readLine()) != null ) {
-                text = lineBuffer ;
+    }
+
+    public void checkPermission() {
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED){
+            setUpReadWriteExternalStorage();
+        } else {
+            requestLocationPermission();
+        }
+
+    }
+
+    private void requestLocationPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+
+        } else {
+            Toast toast =
+                    Toast.makeText(this, "アプリ実行に許可が必要です", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,},
+                    REQUEST_PERMISSION);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setUpReadWriteExternalStorage();
+            } else {
+                Toast toast =
+                        Toast.makeText(this, "何もできません", Toast.LENGTH_SHORT);
+                toast.show();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-        return text;
     }
-
 }
