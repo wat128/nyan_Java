e0e634520ec8b7c422d08944e553d4b0ebacd2f5
wat128 <wat128sideb@gmail.com>
2020-07-20 11:43:16 +0900

    20.Data Storage データベース SQLiteの簡単作成

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -1,43 +1,22 @@
 package com.wat128.nyan_java;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Build;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-    private ImageView imageView;
-
-    private final int REQUEST_PERMISSION = 1000;
-
-    private String fileName = "image3.jpeg";
-    private File file;
+    private EditText editTextKey, editTextValue;
+    private TestOpenHelper helper;
+    private SQLiteDatabase db;
 
 
     @Override
@@ -45,115 +24,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File path = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        file = new File(path, fileName);
+        editTextKey = findViewById(R.id.edit_text_key);
+        editTextValue = findViewById(R.id.edit_text_value);
 
         textView = findViewById(R.id.text_view);
-        String str = "image file: "+fileName;
-        textView.setText(str);
 
-        imageView = findViewById(R.id.image_view);
-
-
-        if(Build.VERSION.SDK_INT >= 23)
-            checkPermission();
-        else
-            setUpWriteExternalStorage();
-
-    }
-
-    private void setUpWriteExternalStorage(){
-        Button buttonSave = findViewById(R.id.button_save);
-        buttonSave.setOnClickListener(new View.OnClickListener(){
+        Button insertButton = findViewById(R.id.button_insert);
+        insertButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
+            public void onClick(View v) {
 
-                try(
-                    InputStream inputStream =
-                            getResources().getAssets().open(fileName);
-                    FileOutputStream output =
-                            new FileOutputStream(file)) {
+                if(helper == null)
+                    helper = new TestOpenHelper(getApplicationContext());
 
-                    int DEFAULT_BUFFER_SIZE = 10240 * 4;
-                    byte[] buf = new byte[DEFAULT_BUFFER_SIZE];
-                    int len;
+                if(db == null)
+                    db = helper.getWritableDatabase();
 
-                    while((len=inputStream.read(buf))!=-1){
-                        output.write(buf,0,len);
-                    }
-                    output.flush();
+                String key = editTextKey.getText().toString();
+                String value = editTextValue.getText().toString();
 
-                    textView.setText(R.string.saved);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    textView.setText(R.string.error);
-                }
-
+                insertData(db, key, Integer.valueOf(value));
             }
         });
 
-        Button buttonRead = findViewById(R.id.button_read);
-        buttonRead.setOnClickListener(new View.OnClickListener(){
+        Button readButton = findViewById(R.id.button_read);
+        readButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-
-                try(InputStream inputStream0 =
-                            new FileInputStream(file) ) {
-
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream0);
-                    imageView.setImageBitmap(bitmap);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+            public void onClick(View v) {
+                readData();
             }
         });
     }
 
-    public void checkPermission() {
 
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED){
-            setUpWriteExternalStorage();
+    private void readData(){
+
+        if(helper == null)
+            helper = new TestOpenHelper(getApplicationContext());
+
+        if(db == null)
+            db = helper.getReadableDatabase();
+
+        Log.d("debug","**********Cursor");
+
+        Cursor cursor = db.query(
+                "testdb",
+                new String[] { "company", "stockprice" },
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append(": ");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("\n");
+            cursor.moveToNext();
+
         }
-        else{
-            requestLocationPermission();
-        }
+
+        cursor.close();
+
+        Log.d("debug","**********"+sbuilder.toString());
+        textView.setText(sbuilder.toString());
 
     }
 
-    private void requestLocationPermission() {
+    private void insertData(SQLiteDatabase db, String com, int price){
 
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    REQUEST_PERMISSION);
-        } else {
-            Toast toast = Toast.makeText(this, "許可してください", Toast.LENGTH_SHORT);
-            toast.show();
+        ContentValues values = new ContentValues();
+        values.put("company", com);
+        values.put("stockprice", price);
 
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,},
-                    REQUEST_PERMISSION);
-        }
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if (requestCode == REQUEST_PERMISSION) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setUpWriteExternalStorage();
-            } else {
-                Toast toast = Toast.makeText(this, "何もできません", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        }
+        db.insert("testdb", null, values);
 
     }
 }
