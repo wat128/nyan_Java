95dcc003e6f0067451efa3b8d64594de768d19af
wat128 <wat128sideb@gmail.com>
2020-07-20 10:59:27 +0900

    20.Data Storage 外部ストレージに画像を保存・読出しする

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -6,12 +6,15 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,123 +23,102 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private ImageView imageView;
+
     private final int REQUEST_PERMISSION = 1000;
 
-    private TextView textView;
-    private EditText editText;
+    private String fileName = "image3.jpeg";
     private File file;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File path = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
-        String testfile ="testfile.txt";
-        file = new File(path, testfile);
-
-        if (Build.VERSION.SDK_INT >= 23)
-            checkPermission();
-        else
-            setUpReadWriteExternalStorage();
-    }
-
-    private void setUpReadWriteExternalStorage(){
+        File path = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        file = new File(path, fileName);
 
         textView = findViewById(R.id.text_view);
-        editText = findViewById(R.id.edit_text);
+        String str = "image file: "+fileName;
+        textView.setText(str);
 
+        imageView = findViewById(R.id.image_view);
+
+
+        if(Build.VERSION.SDK_INT >= 23)
+            checkPermission();
+        else
+            setUpWriteExternalStorage();
+
+    }
+
+    private void setUpWriteExternalStorage(){
         Button buttonSave = findViewById(R.id.button_save);
         buttonSave.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View arg0) {
-                if(isExternalStorageWritable()) {
-                    String text = null;
-                    String str = editText.getText().toString();
 
-                    try(FileOutputStream fileOutputStream =
-                                new FileOutputStream(file, true);
-                        OutputStreamWriter outputStreamWriter =
-                                new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
-                        BufferedWriter bw =
-                                new BufferedWriter(outputStreamWriter);
-                    ) {
+                try(
+                    InputStream inputStream =
+                            getResources().getAssets().open(fileName);
+                    FileOutputStream output =
+                            new FileOutputStream(file)) {
 
-                        bw.write(str);
-                        bw.flush();
-                        text = "saved";
+                    int DEFAULT_BUFFER_SIZE = 10240 * 4;
+                    byte[] buf = new byte[DEFAULT_BUFFER_SIZE];
+                    int len;
 
-                    } catch (Exception e) {
-
-                        text = "error: FileOutputStream";
-                        e.printStackTrace();
-
+                    while((len=inputStream.read(buf))!=-1){
+                        output.write(buf,0,len);
                     }
+                    output.flush();
 
-                    textView.setText(text);
+                    textView.setText(R.string.saved);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    textView.setText(R.string.error);
                 }
-            };
+
+            }
         });
 
         Button buttonRead = findViewById(R.id.button_read);
         buttonRead.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View arg0) {
-                String str = null;
 
-                if(isExternalStorageReadable()) {
+                try(InputStream inputStream0 =
+                            new FileInputStream(file) ) {
 
-                    try(FileInputStream fileInputStream =
-                                new FileInputStream(file);
-                        InputStreamReader inputStreamReader =
-                                new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
-                        BufferedReader reader=
-                                new BufferedReader(inputStreamReader); ) {
-
-                        String lineBuffer;
-
-                        while( (lineBuffer = reader.readLine()) != null ) {
-                            str = lineBuffer ;
-                        }
-
-                    } catch (Exception e) {
-                        str = "error: FileInputStream";
-                        e.printStackTrace();
-                    }
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream0);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                textView.setText(str);
-            };
+
+            }
         });
-    }
-
-    public boolean isExternalStorageWritable() {
-
-        String state = Environment.getExternalStorageState();
-        return (Environment.MEDIA_MOUNTED.equals(state));
-
-    }
-
-    public boolean isExternalStorageReadable() {
-
-        String state = Environment.getExternalStorageState();
-        return (Environment.MEDIA_MOUNTED.equals(state) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
-
     }
 
     public void checkPermission() {
 
         if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-                PackageManager.PERMISSION_GRANTED){
-            setUpReadWriteExternalStorage();
-        } else {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED){
+            setUpWriteExternalStorage();
+        }
+        else{
             requestLocationPermission();
         }
 
@@ -147,11 +129,10 @@ public class MainActivity extends AppCompatActivity {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
-
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
         } else {
-            Toast toast =
-                    Toast.makeText(this, "アプリ実行に許可が必要です", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(this, "許可してください", Toast.LENGTH_SHORT);
             toast.show();
 
             ActivityCompat.requestPermissions(this,
@@ -167,12 +148,12 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == REQUEST_PERMISSION) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setUpReadWriteExternalStorage();
+                setUpWriteExternalStorage();
             } else {
-                Toast toast =
-                        Toast.makeText(this, "何もできません", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(this, "何もできません", Toast.LENGTH_SHORT);
                 toast.show();
             }
         }
+
     }
 }
