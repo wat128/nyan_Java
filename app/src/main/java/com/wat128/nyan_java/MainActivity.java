5522464d47827a543824c1f39c155ea83e6ec3f1
wat128 <wat128sideb@gmail.com>
2020-07-20 12:31:16 +0900

    23.Timer タイマーカウンターをChronometerで簡単に作成する

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -5,106 +5,43 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
-    private EditText editTextKey, editTextValue;
-    private TestOpenHelper helper;
-    private SQLiteDatabase db;
-
+    private Chronometer chronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextKey = findViewById(R.id.edit_text_key);
-        editTextValue = findViewById(R.id.edit_text_value);
+        chronometer = findViewById(R.id.chronometer);
 
-        textView = findViewById(R.id.text_view);
+        Button startButton = findViewById(R.id.start_button);
+        Button stopButton = findViewById(R.id.stop_button);
 
-        Button insertButton = findViewById(R.id.button_insert);
-        insertButton.setOnClickListener(new View.OnClickListener() {
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if(helper == null)
-                    helper = new TestOpenHelper(getApplicationContext());
-
-                if(db == null)
-                    db = helper.getWritableDatabase();
-
-                String key = editTextKey.getText().toString();
-                String value = editTextValue.getText().toString();
-
-                insertData(db, key, Integer.valueOf(value));
+                // set the base to the current time just before calling start()
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
             }
         });
 
-        Button readButton = findViewById(R.id.button_read);
-        readButton.setOnClickListener(new View.OnClickListener() {
+        stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                readData();
+                chronometer.stop();
             }
         });
     }
 
-
-    private void readData(){
-
-        if(helper == null)
-            helper = new TestOpenHelper(getApplicationContext());
-
-        if(db == null)
-            db = helper.getReadableDatabase();
-
-        Log.d("debug","**********Cursor");
-
-        Cursor cursor = db.query(
-                "testdb",
-                new String[] { "company", "stockprice" },
-                null,
-                null,
-                null,
-                null,
-                null
-        );
-
-        cursor.moveToFirst();
-
-        StringBuilder sbuilder = new StringBuilder();
-
-        for (int i = 0; i < cursor.getCount(); i++) {
-
-            sbuilder.append(cursor.getString(0));
-            sbuilder.append(": ");
-            sbuilder.append(cursor.getInt(1));
-            sbuilder.append("\n");
-            cursor.moveToNext();
-
-        }
-
-        cursor.close();
-
-        Log.d("debug","**********"+sbuilder.toString());
-        textView.setText(sbuilder.toString());
-
-    }
-
-    private void insertData(SQLiteDatabase db, String com, int price){
-
-        ContentValues values = new ContentValues();
-        values.put("company", com);
-        values.put("stockprice", price);
-
-        db.insert("testdb", null, values);
-
-    }
 }
