1e3f2a6ccb6bf8b0d7b629e8488497a64aabdb22
wat128 <wat128sideb@gmail.com>
2020-07-29 16:28:57 +0900

    27.Audio SoundPoolで効果音を鳴らす

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -1,124 +1,71 @@
 package com.wat128.nyan_java;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.media.AudioAttributes;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
-import android.media.MediaPlayer;
+
+import android.media.SoundPool;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.os.Handler;
-import android.os.SystemClock;
+
 import android.util.Log;
 import android.view.View;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
-import android.widget.Chronometer;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
-import java.util.Timer;
-import java.util.TimerTask;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity{
-
-    private static final int SamplingRate = 33100;
+    private SoundPool soundPool;
+    private int soundOne, soundTwo;
+    private Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonStart = findViewById(R.id.button_start);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setAudioAttributes(audioAttributes)
+                .setMaxStreams(2)
+                .build();
+
+        soundOne = soundPool.load(this, R.raw.one, 1);
+        soundTwo = soundPool.load(this, R.raw.two, 1);
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                Log.d("debug","sampleId="+sampleId);
+                Log.d("debug","status="+status);
+            }
+        });
+
+        button1 = findViewById(R.id.button_start);
+        button2 = findViewById(R.id.button_clear);
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wavPlay();
+                soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1);
+
+                RotateAnimation buttonRotation = new RotateAnimation(0, 360, button1.getWidth()/2, button1.getHeight()/2);
+                buttonRotation.setDuration(2000);
+                button1.startAnimation(buttonRotation);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                soundPool.play(soundTwo, 1.0f, 1.0f, 1, 0, 1);
+
+                RotateAnimation buttonRotation = new RotateAnimation(0, 360, button2.getWidth()/2, button2.getHeight()/2);
+                buttonRotation.setDuration(2000);
+                button2.startAnimation(buttonRotation);
             }
         });
     }
-
-    private void wavPlay() {
-        InputStream input = null;
-        byte[] wavData = null;
-
-        try {
-
-            input = getResources().openRawResource(R.raw.sample);
-            wavData = new byte[input.available()];
-
-            String readBytes = String.format(
-                    Locale.US, "read bytes = %d",input.read(wavData));
-            Log.d("debug",readBytes);
-            input.close();
-
-        } catch (FileNotFoundException fne) {
-
-            fne.printStackTrace();
-
-        } catch (IOException ioe) {
-
-            ioe.printStackTrace();
-            Log.d("debug", "error");
-
-        } finally{
-
-            try{
-
-                if(input != null)
-                    input.close();
-
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-
-        }
-
-
-
-        int bufSize = android.media.AudioTrack.getMinBufferSize(
-                SamplingRate,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT);
-
-        AudioTrack audioTrack = new AudioTrack.Builder()
-                .setAudioAttributes(new AudioAttributes.Builder()
-                        // setUsage()は音声ストリームの指定と考えられるため、USAGE_VOICE_COMMUNICATIONの際は
-                        // 着信・通知の音量を使用することになる。USAGE_MEDIAはメディアの音量が使用される。
-                        .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                        .build())
-                .setAudioFormat(new AudioFormat.Builder()
-                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                        .setSampleRate(SamplingRate)
-                        .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
-                        .build())
-                .setBufferSizeInBytes(bufSize)
-                .build();
-
-        audioTrack.play();
-
-        audioTrack.write(wavData, 44, wavData.length - 44);
-    }
-
-
 }
