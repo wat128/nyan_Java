d7dec976d9a4869518aa6c4c794b9aa50dfd83c0
wat128 <wat128sideb@gmail.com>
2020-07-30 12:41:07 +0900

    29.Layout Theme.NoTitleBar 全画面表示、タイトルバーを非表示 - 鑑賞モード

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wat128.nyan_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.media.AudioAttributes;
 
 import android.media.SoundPool;
@@ -12,60 +14,32 @@ import android.view.animation.RotateAnimation;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    private SoundPool soundPool;
-    private int soundOne, soundTwo;
-    private Button button1, button2;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                .build();
+        leanbackMode();
+    }
 
-        soundPool = new SoundPool.Builder()
-                .setAudioAttributes(audioAttributes)
-                .setMaxStreams(2)
-                .build();
+    private void leanbackMode() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
-        soundOne = soundPool.load(this, R.raw.one, 1);
-        soundTwo = soundPool.load(this, R.raw.two, 1);
-        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-            @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                Log.d("debug","sampleId="+sampleId);
-                Log.d("debug","status="+status);
-            }
-        });
+        decorView.setOnSystemUiVisibilityChangeListener(
+                new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                            Log.d("debug", "The system bars are visible");
+                        else
+                            Log.d("debug", "The system bars are not visible");
 
-        button1 = findViewById(R.id.button_start);
-        button2 = findViewById(R.id.button_clear);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1);
-
-                RotateAnimation buttonRotation = new RotateAnimation(0, 360, button1.getWidth()/2, button1.getHeight()/2);
-                buttonRotation.setDuration(2000);
-                button1.startAnimation(buttonRotation);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                soundPool.play(soundTwo, 1.0f, 1.0f, 1, 0, 1);
-
-                RotateAnimation buttonRotation = new RotateAnimation(0, 360, button2.getWidth()/2, button2.getHeight()/2);
-                buttonRotation.setDuration(2000);
-                button2.startAnimation(buttonRotation);
-            }
-        });
+                    }
+                }
+        );
     }
 }
