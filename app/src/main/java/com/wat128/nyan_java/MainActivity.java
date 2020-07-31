7e72732e21aaab6f2daa4a45f49cd75bceefef01
wat128 <wat128sideb@gmail.com>
2020-07-31 16:25:58 +0900

    30.Canvas CanvasとPaintで円や矩形を描く - Canvasの基本的な形

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -3,6 +3,10 @@ package com.wat128.nyan_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.media.AudioAttributes;
 
 import android.media.SoundPool;
@@ -12,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.ScrollView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +24,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        MyView myView = new MyView(this);
+        setContentView(myView);
+    }
+
+    class MyView extends View {
+        Paint paint;
+
+        float StrokeWidth = 20.0f;
+
+        public MyView(Context context) {
+            super(context);
+            paint = new Paint();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+
+            paint.setColor(Color.argb(255,255,0,255));
+            paint.setStrokeWidth(StrokeWidth);
+            paint.setStyle(Paint.Style.STROKE);
+
+            canvas.drawRect(300,300,600,600,paint);
+        }
     }
 }
