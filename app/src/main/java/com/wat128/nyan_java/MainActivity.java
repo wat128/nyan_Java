3d0a5ca0ab2afce7219d029f263faf604aa37375
wat128 <wat128sideb@gmail.com>
2020-08-02 16:29:04 +0900

    30.Canvas Canvasで画像と文字を表示する - Canvasのスクロール

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 
@@ -15,29 +16,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        TextView label = this.findViewById(R.id.label);
-        label.setText(R.string.text);
+        MyView myView = new MyView(this);
+        ScrollView scrollView = new ScrollView(this);
 
-        myView = this.findViewById(R.id.my_view);
-        myView.showCanvas(true);
-
-        showCanvas = true;
-
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(showCanvas) {
-                    myView.showCanvas(false);
-                    showCanvas = false;
-                }
-                else {
-                    myView.showCanvas(true);
-                    showCanvas = true;
-                }
-            }
-        });
+        scrollView.addView(myView);
+        setContentView(scrollView);
     }
 }
--- app/src/main/java/com/wat128/nyan_java/MyView.java
+++ app/src/main/java/com/wat128/nyan_java/MyView.java
@@ -16,35 +16,27 @@ import android.view.View;
 public class MyView extends View {
 
     private Paint paint;
-    private Boolean viewflg;
     private Bitmap bmp;
 
-    public MyView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+    public MyView(Context context) {
+        super(context);
         paint = new Paint();
-        viewflg = true;
         bmp = BitmapFactory.decodeResource(getResources(), R.drawable.image);
     }
 
-    public void showCanvas(boolean flg) {
-        viewflg = flg;
-        invalidate();
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = 10000;
+        setMeasuredDimension(width, height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         Log.d("TestView", "onDraw()");
         canvas.drawColor(Color.argb(255, 0, 0, 125));
-        if(viewflg) {
-            canvas.drawBitmap(bmp, 80, 200, paint);
-        }
-        else {
-            paint.setStyle(Paint.Style.FILL_AND_STROKE);
-            paint.setStrokeWidth(5);
-            paint.setTextSize(100);
-            paint.setColor(Color.argb(255,255,255,0));
-            canvas.drawText("Kina",120,600, paint);
-        }
+
+        canvas.drawBitmap(bmp, 50, 300, paint);
 
         // Circle
         paint.setColor(Color.argb(255,255,0,255));
@@ -52,13 +44,13 @@ public class MyView extends View {
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
 
-        canvas.drawCircle(130, 150,100, paint);
+        canvas.drawCircle(130, 550,100, paint);
 
         // Circle2
         paint.setStrokeWidth(30);
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
-        canvas.drawCircle(130,150,50, paint);
+        canvas.drawCircle(130,550,50, paint);
 
     }
 }
