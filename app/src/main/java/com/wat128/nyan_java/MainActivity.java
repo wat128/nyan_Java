9cb4a674326ac7a05c2c01f20e0ca7466c08a158
wat128 <wat128sideb@gmail.com>
2020-08-02 13:46:03 +0900

    30.Canvas CanvasをClearして再描画

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -26,6 +26,9 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean showCanvas;
+    private MyView myView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,5 +36,27 @@ public class MainActivity extends AppCompatActivity {
 
         TextView label = this.findViewById(R.id.label);
         label.setText(R.string.text);
+
+        Log.d("MainActivity", "onCreate()");
+        myView = this.findViewById(R.id.my_view);
+        myView.showCanvas(true);
+        showCanvas = true;
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("debug", "button on click");
+                if(showCanvas) {
+                    myView.showCanvas(false);
+                    showCanvas = false;
+                    Log.d("debug", "showCanvas = false");
+                } else {
+                    myView.showCanvas(true);
+                    showCanvas = true;
+                    Log.d("debug", "showCanvas = true");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/wat128/nyan_java/MyView.java
+++ app/src/main/java/com/wat128/nyan_java/MyView.java
@@ -4,43 +4,62 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Picture;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class MyView extends View {
 
-    Paint paint;
+    private Paint paint;
+    private Path path;
+    private Boolean viewflg;
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         paint = new Paint();
+        path = new Path();
+        viewflg = true;
+    }
+
+    public void showCanvas(boolean flg) {
+        viewflg = flg;
+        invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.d("TestView", "onDraw()");
 
-        canvas.drawColor(Color.argb(255, 0, 0, 125));
+        if(viewflg) {
+            canvas.drawColor(Color.argb(255, 0, 0, 125));
 
 
-        // Circle
-        paint.setColor(Color.argb(255,125,125,255));
-        paint.setStrokeWidth(30);
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
+            // Circle
+            paint.setColor(Color.argb(255,125,125,255));
+            paint.setStrokeWidth(30);
+            paint.setAntiAlias(true);
+            paint.setStyle(Paint.Style.STROKE);
 
-        canvas.drawCircle(450,450,100, paint);
+            canvas.drawCircle(450,450,100, paint);
 
-        // Rect
-        paint.setColor(Color.argb(255,255,0,255));
-        paint.setStrokeWidth(10);
-        paint.setStyle(Paint.Style.STROKE);
-        canvas.drawRect(480,480,850,880, paint);
+            // Rect
+            paint.setColor(Color.argb(255,255,0,255));
+            paint.setStrokeWidth(10);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(480,480,850,880, paint);
 
-        // Line
-        paint.setStrokeWidth(15);
-        paint.setColor(Color.argb(255,0,255,0));
-        canvas.drawLine(350,850,750,630, paint);
+            // Line
+            paint.setStrokeWidth(15);
+            paint.setColor(Color.argb(255,0,255,0));
+            canvas.drawLine(350,850,750,630, paint);
 
+        } else {
+            Log.d("TestView", " viewflg = false");
+
+            canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        }
     }
 }
