a415cc36ec787da46455ce3f19180e38871c990b
wat128 <wat128sideb@gmail.com>
2020-08-03 18:44:15 +0900

    31.onTouchEvent タッチイベントを拾う

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
@@ -15,37 +16,75 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Arc arc;
-    private int endAngle = 0;
-    final private int animationPeriod = 2000;
-
+    private StringBuffer info = new StringBuffer("Test onTouchEvent\n\n");
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        endAngle = 88 * 360 / 100;
+        textView = findViewById(R.id.text_view);
 
-        arc = findViewById(R.id.arc);
-
-        Button buttonStart = findViewById(R.id.button_start);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AnimationArc animation = new AnimationArc(arc, endAngle);
-                animation.setDuration(animationPeriod);
-                arc.startAnimation(animation);
+                info.delete(0, info.length());
+                textView.setText(info);
             }
         });
+    }
 
-        Button buttonReset = findViewById(R.id.button_reset);
-        buttonReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AnimationArc animation = new AnimationArc(arc, 0);
-                animation.setDuration(0);
-                arc.startAnimation(animation);
-            }
-        });
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+
+                info.append("ACTION_DOWN\n");
+                info.append("Pressure");
+                info.append(event.getPressure());
+                info.append("\n");
+                info.append("x1 : ");
+                info.append(event.getX());
+                info.append("\n");
+                info.append("y1 : ");
+                info.append(event.getY());
+                info.append("\n");
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+
+                info.append("ACTION_UP\n");
+                info.append("x2 : ");
+                info.append(event.getX());
+                info.append("\n");
+                info.append("y2 : ");
+                info.append(event.getY());
+                info.append("\n");
+                long eventDuration2 = event.getEventTime() - event.getDownTime();
+                info.append("duration : ");
+                info.append(eventDuration2);
+                info.append(" msec\n\n");
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                info.append("ACTION_MOVE\n");
+
+                break;
+            case MotionEvent.ACTION_CANCEL:
+
+                info.append("ACTION.CANCEL\n");
+
+                break;
+            default:
+                break;
+        }
+
+        textView.setText(info);
+
+        return false;
     }
 }
