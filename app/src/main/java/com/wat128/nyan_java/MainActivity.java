30164a1315a92d104b85ac26d272dbbc239c3c16
wat128 <wat128sideb@gmail.com>
2020-08-06 15:18:21 +0900

    34.Intent Storage Access Frameworkで画像を取り出す

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -3,10 +3,12 @@ package com.wat128.nyan_java;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -18,6 +20,7 @@ import android.media.MediaSession2Service;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
@@ -34,6 +37,8 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import org.w3c.dom.Text;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -41,31 +46,64 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RESULT_PICK_IMAGEFILE = 1001;
+    private TextView textView;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.text_view);
+        imageView = findViewById(R.id.image_view);
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                callMailer();
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+
+                startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
             }
         });
     }
 
-    private void callMailer() {
-        String[] addresses = {"xxx@yyy.zzz"};
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:"));
-        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
-        intent.putExtra(Intent.EXTRA_SUBJECT, "test mail");
-        intent.putExtra(Intent.EXTRA_TEXT, "本文");
+        if(requestCode == RESULT_PICK_IMAGEFILE && resultCode == Activity.RESULT_OK) {
 
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+            if(data.getData() != null) {
+
+                ParcelFileDescriptor pfDesctiptor = null;
+                try{
+                    Uri uri = data.getData();
+
+                    textView.setText(String.format(Locale.US, "Uri: %s",uri.toString()));
+
+                    pfDesctiptor = getContentResolver().openFileDescriptor(uri, "r");
+                    if(pfDesctiptor != null) {
+                        FileDescriptor fileDescriptor = pfDesctiptor.getFileDescriptor();
+                        Bitmap bmp = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                        pfDesctiptor.close();
+                        imageView.setImageBitmap(bmp);
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                } finally {
+                    try{
+                        if(pfDesctiptor != null) {
+                            pfDesctiptor.close();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
 }
