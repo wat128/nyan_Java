69c7842e58f06f479bae565f5fa0aab75326488c
wat128 <wat128sideb@gmail.com>
2020-08-06 17:39:45 +0900

    35.WebView WebViewの設定 - error

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -2,111 +2,70 @@ package com.wat128.nyan_java;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.BitmapCompat;
-
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.media.MediaSession2Service;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.ParcelFileDescriptor;
-import android.provider.MediaStore;
-import android.speech.RecognizerIntent;
-import android.view.MotionEvent;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-
-import org.w3c.dom.Text;
-
-import java.io.FileDescriptor;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE= 1000;
-    private TextView textView;
-
-    private int lang;
+    private WebView webView;
+    private String accessUrl = "https://google.com";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lang = 0;
+        Button buttonWebView = findViewById(R.id.button_01);
+        Button buttonBrowser = findViewById(R.id.button_02);
 
-        textView = findViewById(R.id.text_view);
-
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonWebView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                speech();
+
+                setContentView(R.layout.web);
+                webView = findViewById(R.id.web_view);
+
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.getSettings().setDomStorageEnabled(true);
+
+                getWindow().setFlags(
+                        WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                        WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+
+                webView.loadUrl(accessUrl);
+            }
+        });
+
+        buttonBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(accessUrl);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
             }
         });
     }
 
-    private void speech() {
-
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-        if(lang == 0)
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.JAPAN.toString());
-        else if (lang == 1)
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH.toString());
-        else if (lang == 2)
-            intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true);
-        else
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-
-        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 100);
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "音声を入力");
-
-        try{
-            startActivityForResult(intent, REQUEST_CODE);
-        } catch (ActivityNotFoundException e) {
-            e.printStackTrace();
-            textView.setText(R.string.error);
-        }
-    }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            ArrayList<String> candidates = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        if( (event.getAction() == KeyEvent.ACTION_DOWN )
+                && (keyCode == KeyEvent.KEYCODE_BACK) ){
 
-            if(candidates.size() > 0)
-                textView.setText(candidates.get(0));
+            if(webView.canGoBack())
+                webView.goBack();
+            else
+                finish();
         }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
