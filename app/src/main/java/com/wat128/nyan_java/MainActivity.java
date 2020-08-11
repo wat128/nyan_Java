83bd5bfcaf50b2a6894c1fe453d7095d39624b52
wat128 <wat128sideb@gmail.com>
2020-08-11 15:43:18 +0900

    38.Animation AnimationSet アニメーションの組み合わせ - コードで記述

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
@@ -31,7 +32,6 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +39,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.carlos);
-        animation = AnimationUtils.loadAnimation(this, R.anim.animation_set);
 
-        Button button_rotate = findViewById(R.id.button);
-        button_rotate.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                imageView.startAnimation(animation);
+                setAnime();
             }
         });
+    }
+
+    private void setAnime() {
+        ScaleAnimation scaleAnime = new ScaleAnimation(
+                1.0f, 0.0f, 1.0f, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnime.setDuration(2000);
+
+        RotateAnimation rotateAnime = new RotateAnimation(0.0f, 120.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.5f);
+        rotateAnime.setDuration(2000);
+
+        AnimationSet animeSet = new AnimationSet(true);
+        animeSet.addAnimation(scaleAnime);
+        animeSet.addAnimation(rotateAnime);
+
+        imageView.startAnimation(animeSet);
     }
 }
