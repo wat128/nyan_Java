8ef281471097843678c44c4905f75b6ddefd46e4
wat128 <wat128sideb@gmail.com>
2020-08-11 14:58:23 +0900

    38.Animation TranslateAnimation 移動アニメーション - コードで記述

--- app/src/main/java/com/wat128/nyan_java/MainActivity.java
+++ app/src/main/java/com/wat128/nyan_java/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -18,6 +19,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -29,7 +31,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private Animation animation;
+    private TranslateAnimation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +39,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.carlos);
-        animation = AnimationUtils.loadAnimation(this, R.anim.translate_animation);
 
         Button button_rotate = findViewById(R.id.button);
         button_rotate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                imageView.startAnimation(animation);
+                startTranslate();
             }
         });
+    }
+
+    private void startTranslate() {
+
+        int type = 0;
+
+        if(type == 0) {
+            animation = new TranslateAnimation(
+                    Animation.ABSOLUTE, 0.0f,
+                    Animation.ABSOLUTE, 500.0f,
+                    Animation.ABSOLUTE, 0.0f,
+                    Animation.ABSOLUTE, 1200.f);
+        }
+        else if(type == 1) {
+            animation = new TranslateAnimation(
+                    Animation.RELATIVE_TO_SELF, 0.0f,
+                    Animation.RELATIVE_TO_SELF, 0.9f,
+                    Animation.RELATIVE_TO_SELF, 0.0f,
+                    Animation.RELATIVE_TO_SELF, 1.8f);
+        }
+        else if(type == 2) {
+            animation = new TranslateAnimation(
+                    Animation.RELATIVE_TO_PARENT, 0.0f,
+                    Animation.RELATIVE_TO_PARENT, 0.4f,
+                    Animation.RELATIVE_TO_PARENT, 0.0f,
+                    Animation.RELATIVE_TO_PARENT, 0.6f);
+        }
+
+        animation.setDuration(2000);
+        animation.setRepeatCount(0);
+        animation.setFillAfter(true);
+
+        imageView.startAnimation(animation);
     }
 }
